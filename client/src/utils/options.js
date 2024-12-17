9329fc3f456f8c85d54652dd2590a4689cef70fe
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-17 17:33:56 -0300

    front: dash options

---
+++ client/src/utils/options.js
@@ -0,0 +1,22 @@
+const options = [
+  {
+    name: 'Gamification',
+    activeScreen: 'category',
+    category: 'gamification',
+    icon:'/gamification.svg'
+  },
+  
+  {
+    name: 'Jornadas',
+    activeScreen: 'category',
+    category: 'journey',
+    icon:'/journey.svg'
+  },
+  {
+    name: 'Campanhas',
+    activeScreen: 'category',
+    category: 'campaign',
+    icon:'/campaign.svg'
+  }
+]
+export default options
