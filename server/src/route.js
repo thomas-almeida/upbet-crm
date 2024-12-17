dd715570227d6e833c1fbf351972773375fef418
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-17 17:34:54 -0300

    back: new routes

--- server/src/route.js
+++ server/src/route.js
@@ -1,10 +1,17 @@
 import { Router } from "express"
 import queryDataController from "../controllers/queryDataController.js"
+import userController from "../controllers/userController.js"
 
 const api = Router()
 
 // get query results (json)
 api.post('/get-query-results', queryDataController.convertCSVQuery)
-api.get('/get-dash-data/:id', queryDataController.getDashData)
+api.get('/get-dash-data', queryDataController.getDashData)
+api.get('/get-dash-by-id/:id', queryDataController.getDashById)
+
+// user
+api.post('/user/sign-up', userController.signUp)
+api.post('/user/sign-in', userController.signIn)
+api.get('/user/get-user-by-id/:id', userController.getUserById)
 
 export default api
