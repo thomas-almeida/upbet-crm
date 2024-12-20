45fd3f4065762859e3f4faf05875f45a545fea06
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-20 00:51:06 -0300

    Front: update baseUlr to prod

--- client/src/service/index.js
+++ client/src/service/index.js
@@ -2,22 +2,40 @@ import axios from "axios"
 import baseUrl from '../utils/baseUrl.js'
 
 const signIn = async (payload) => {
-  const response = await axios.post(`${baseUrl.localhost}/user/sign-in`, payload)
+  const response = await axios.post(`${baseUrl.production}/user/sign-in`, payload)
   return response.data
 }
 
 const getUserById = async (userId) => {
-  const response = await axios.get(`${baseUrl.localhost}/user/get-user-by-id/${userId}`)
+  const response = await axios.get(
+    `${baseUrl.production}/user/get-user-by-id/${userId}`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true"
+    }
+  }
+  )
   return response.data
 }
 
 const getDashData = async () => {
-  const response = await axios.get(`${baseUrl.localhost}/get-dash-data`)
+  const response = await axios.get(
+    `${baseUrl.production}/get-dash-data`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true"
+    }
+  }
+  )
   return response.data
 }
 
 const getDashById = async (dashId) => {
-  const response = await axios.get(`${baseUrl.localhost}/get-dash-by-id/${dashId}`)
+  const response = await axios.get(
+    `${baseUrl.production}/get-dash-by-id/${dashId}`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true"
+    }
+  }
+  )
   return response.data
 }
 
--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const localhost = 'http://localhost:3002'
-const production = ''
+const production = 'https://workable-sloth-strangely.ngrok-free.app/upbet'
 
 export default {
   localhost,
