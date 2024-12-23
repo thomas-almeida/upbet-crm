85b9a4e0a0b429194d7642f83ac811fec819b1b4
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-23 18:03:32 -0300

    back: data from sheets api to campaigns

--- server/controllers/queryDataController.js
+++ server/controllers/queryDataController.js
@@ -1,7 +1,7 @@
 import idGenerator from "../utils/idGenerator.js"
-import fs, { futimesSync } from 'fs'
+import fs from 'fs'
 import csvParser from 'csv-parser'
-
+import { google } from 'googleapis'
 import path from "path"
 import { fileURLToPath } from "url"
 
@@ -102,8 +102,99 @@ async function getDashData(req, res) {
   }
 }
 
+async function getSheetsData(req, res) {
+  try {
+
+    const {
+      spreadsheetId,
+      range,
+      createdAt,
+      dashName,
+      metricLabel,
+      label,
+      value,
+      headers,
+      filter,
+      chartType,
+    } = req.body
+    const dbData = fs.readFileSync(dashDB, 'utf-8')
+    dashes = dbData ? JSON.parse(dbData) : []
+
+    if (!spreadsheetId || !range) {
+      return res.status(400).json({
+        message: 'SpreadsheetId or range not defined'
+      })
+    }
+
+    const auth = new google.auth.GoogleAuth({
+      keyFile: path.join(__dirname, '../utils/sheets-credentials.json'),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    })
+
+    const client = await auth.getClient()
+    const sheets = google.sheets({ version: 'v4', auth: client })
+
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range
+    })
+
+    const rows = response.data.values
+
+    if (!rows.length) {
+      return res.status(200).json({
+        message: 'nenhum dado encontrado'
+      })
+    }
+
+    const [sheetHeaders, ...data] = rows
+    const jsonData = data.map(row => {
+      return sheetHeaders.reduce((acc, header, index) => {
+      
+        let value = row[index] || null
+
+        if (typeof value === 'string' && value.includes('R$')) {
+          value = parseFloat(value.replace(/[^\d]/g, ''), 10) || null
+        }
+        acc[header] = value
+        return acc
+      }, {})
+    })
+
+    const resultsData = {
+      id: idGenerator.generateExtensiveId(dashes),
+      name: dashName,
+      createdAt: createdAt,
+      metricLabel: metricLabel,
+      chartType: chartType,
+      filter: filter,
+      guides: {
+        label: label,
+        value: value
+      },
+      headers,
+      results: jsonData
+    }
+
+    res.status(200).json({
+      message: 'success',
+      data: resultsData
+    })
+
+    dashes.push(resultsData)
+    fs.writeFileSync(dashDB, JSON.stringify(dashes, null, 2))
+
+  } catch (error) {
+    res.status(500).json({
+      message: `error, ${error}`
+    })
+    console.error(error)
+  }
+}
+
 export default {
   convertCSVQuery,
   getDashById,
-  getDashData
+  getDashData,
+  getSheetsData
 }
