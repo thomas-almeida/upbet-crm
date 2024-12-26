bea057350ebd2c5a00a4da24fbe053f7fde3cb61
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-12-26 17:13:06 -0300

    new service get

--- client/src/service/index.js
+++ client/src/service/index.js
@@ -50,10 +50,23 @@ const getKPIs = async () => {
   return response.data
 }
 
+const getAllCampaings = async () => {
+  const response = await axios.get(
+    `${baseUrl.production}/campaigns/get-all-campaigns`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true"
+    }
+  }
+  )
+
+  return response.data
+}
+
 export default {
   signIn,
   getUserById,
   getDashById,
   getDashData,
-  getKPIs
+  getKPIs,
+  getAllCampaings
 }
