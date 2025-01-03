b3b971f31b017bf59659c010606e0c62b4345f92
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-01-02 21:36:18 -0300

    back: index config

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,20 @@
 import api from "./route.js"
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from "url"
 
+dotenv.config()
 const app = express()
 const PORT = 3001
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.json())
 app.use(cors())
 app.use(api)
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(PORT, () => {
   console.log(`CRM ONLINE`)
